8e0c8c327cdb0961c8c86b85f73c688d8ad203ea
Bogdan <bogdan.stoinel@yahoo.com>
2023-10-29 00:47:30 +0300

    main file created

--- src/Tren/tren.cpp
+++ src/Tren/tren.cpp
@@ -2,6 +2,9 @@
 #include<string.h>
 #include "tren.hpp"
 
+
+//cautam o valoare in vector, daca o gasim, o mutam la final, functia va ajuta in cadrul metodei sterge_vagon
+//Va fi mai usor de sters vagonul daca se afla pe ultima pozitie
 bool find(int* vector, int lungime_vector, int numar){
     int aux,poz;
     bool check=0;
@@ -20,7 +23,10 @@ bool find(int* vector, int lungime_vector, int numar){
     return check;
 }
 
+
+//Consturctorul
 Tren::Tren(char *ruta, int dimensiune_ruta, int nr_vagoane, int *numere_vagoane){
+    std::cout<<"Constructorul a fost apelat\n";
     this->nr_vagoane=nr_vagoane;
     this->dimensiune_ruta=dimensiune_ruta;
     this->numere_vagoane=new int[this->nr_vagoane];
@@ -31,12 +37,16 @@ Tren::Tren(char *ruta, int dimensiune_ruta, int nr_vagoane, int *numere_vagoane)
     strcpy(this->ruta, ruta);
 }
 
+//Destructorul
 Tren::~Tren(){
+    std::cout<<"Destructorul a fost apelat\n";
     delete[] this->ruta;
     delete[] this->numere_vagoane;
 }
 
+//Copy Constructorul
 Tren::Tren(const Tren& vechi){
+    std::cout<<"Copy Constructorul a fost apelat\n";
     this->dimensiune_ruta=vechi.dimensiune_ruta;
     this->nr_vagoane=vechi.nr_vagoane;
     this->numere_vagoane=new int[this->nr_vagoane];
@@ -45,19 +55,27 @@ Tren::Tren(const Tren& vechi){
         this->numere_vagoane[i]=vechi.numere_vagoane[i];
     }
     this->ruta=new char[this->dimensiune_ruta+1];
-    strcpy(this->ruta, ruta);
+    strcpy(this->ruta, vechi.ruta);
 }
 
+//Move Constructorul
 Tren::Tren(Tren&& vechi){
+    std::cout<<"Move Constructorul a fost apelat\n";
     this->dimensiune_ruta=vechi.dimensiune_ruta;
     this->nr_vagoane=vechi.nr_vagoane;
     this->numere_vagoane=vechi.numere_vagoane;
     this->ruta=vechi.ruta;
     vechi.numere_vagoane=nullptr;
     vechi.ruta=nullptr;
+    vechi.nr_vagoane=0;
+    vechi.dimensiune_ruta=-1;
 }
 
 void Tren::adauga_vagon(int numar_vagon){
+    if(this->ruta==NULL){
+        std::cout<<"Move constructorul a fost apelat pentru acest tren\n";
+        return;
+    }
     int *aux=new int[this->nr_vagoane+1];
     for(int i=0;i<this->nr_vagoane;i++){
         aux[i]=this->numere_vagoane[i];
@@ -73,6 +91,10 @@ void Tren::adauga_vagon(int numar_vagon){
 }
 
 void Tren::schimba_ruta(char *ruta_noua, int dimensiune_ruta){
+    if(this->ruta==NULL){
+        std::cout<<"Move constructorul a fost apelat pentru acest tren\n";
+        return;
+    }
     delete[] this->ruta;
     this->dimensiune_ruta=dimensiune_ruta;
     this->ruta=new char[this->dimensiune_ruta+1];
@@ -80,6 +102,10 @@ void Tren::schimba_ruta(char *ruta_noua, int dimensiune_ruta){
 }
 
 void Tren::sterge_vagon(int numar_vagon){
+    if(this->ruta==NULL){
+        std::cout<<"Move constructorul a fost apelat pentru acest tren\n";
+        return;
+    }
     bool checked=find(this->numere_vagoane, this->nr_vagoane, numar_vagon);
     if(checked==0) return;
     this->nr_vagoane--;
@@ -97,10 +123,14 @@ void Tren::sterge_vagon(int numar_vagon){
 }
 
 void Tren::afisare(){
+    if(this->ruta==NULL){
+        std::cout<<"Move constructorul a fost apelat pentru acest tren\n";
+        return;
+    }
     std::cout<<"Trenul circula pe ruta: "<<this->ruta<<"\n";
-    std::cout<<"Structura: Locomotiva <- ";
+    std::cout<<"Structura: Locomotiva ";
     for(int i=0;i<this->nr_vagoane;i++){
-        std::cout<<this->numere_vagoane[i]<<" <- ";
+        std::cout<<" <- "<<this->numere_vagoane[i];
     }
     std::cout<<"\n";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<string.h>
+#include "tren.hpp"
 
 using namespace std;
 
 int main(){
-    std::cout<<"Hello world!\n";
+    char ruta1[]="Bucuresti-Timisoara", ruta2[]="Bucuresti-Constanta";
+    int dimensiune1=19, dimensiune2=19;
+    int numere_vagoane1[]={434,433,4,3,2,1}, numere_vagoane2[]={1,2,3,4};
+    int nr_vagoane1=6, nr_vagoane2=4;
+    Tren tren1=Tren(ruta1, dimensiune1, nr_vagoane1, numere_vagoane1);
+    tren1.afisare();
+    Tren tren2=Tren(tren1);
+    Tren tren3=std::move(tren2);
+    Tren tren4=Tren(ruta2, dimensiune2, nr_vagoane2, numere_vagoane2);
+    tren2.afisare();
+    tren3.afisare();
+    tren4.afisare();
+    tren1.adauga_vagon(5);
+    tren1.afisare();
+    tren3.sterge_vagon(5);
+    tren3.afisare();
+    tren3.sterge_vagon(2);
+    tren3.afisare();
     return 0;
 }
