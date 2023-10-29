7b86797f9faf6bdfda42f8ccec5751b72594dccf
Bogdan <bogdan.stoinel@yahoo.com>
2023-10-29 12:31:18 +0200

    structura main imbunatatita, incapsulare

--- src/Tren/tren.hpp
+++ src/Tren/tren.hpp
@@ -5,7 +5,8 @@ class Tren
         char *ruta;
         int nr_vagoane;
         int *numere_vagoane;
-    
+        bool find(int numar_vagon);
+        bool find_sterge(int numar_vagon);
     public:
         Tren(char *ruta, int dimensiune_ruta, int nr_vagoane, int *numere_vagoane);
         Tren(const Tren& vechi);
